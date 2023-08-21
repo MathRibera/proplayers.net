3ca0ea33cb13c110f91ddb353e20fb5f414ae47c
Matheus Jorge <bs.fenixfl@gmail.com>
2023-08-21 13:17:11 -0300

    feat: add css

--- frontend/src/app/create/teams/page.tsx
+++ frontend/src/app/create/teams/page.tsx
@@ -6,15 +6,16 @@ export default function Home() {
   const [form, setForm] = useState({
     name: '',
     country: '',
-    image: ''
+    image: '',
+    auth: 'secret'
   })
 
   const createTeam = async (e: any) => {
     e.preventDefault()
-    const res = await fetch('http://localhost:3000/api/teams', {
+    const res = await fetch('http://localhost:3000/api/create/team', {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify(form)
     })
@@ -22,11 +23,16 @@ export default function Home() {
     console.log(data)
   }
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex flex-col items-center">
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-slate-400">
+      <div className="flex flex-col items-center border-2 border-black p-2 rounded-lg">
         <h1>Create Teams</h1>
         <form className="flex flex-col">
-          <label htmlFor="name">Name</label>
+          <label
+            htmlFor="name"
+            className="mb-2 mt-4"
+            >
+            Name
+            </label>
           <input
             type="text"
             id="name"
@@ -36,7 +42,10 @@ export default function Home() {
               (e) => setForm({...form, name: e.target.value})
             }
           />
-          <label htmlFor="country">Country</label>
+          <label
+            htmlFor="country"
+            className="mt-2 mb-2 border-t-2 border-black"
+            >Country</label>
           <input
             type="text"
             id="country"
@@ -46,12 +55,16 @@ export default function Home() {
               (e) => setForm({...form, country: e.target.value})
             }
             />
-          <label htmlFor="image">Logo team URL</label>
+          <label
+            htmlFor="image"
+            className="mb-2 mt-2 border-t-2 border-black"
+            >Logo team URL</label>
           <input
             type="text"
             id="image"
             name="image"
             value={form.image}
+            className='mb-4 w-96 h-20'
             onChange={
               (e) => setForm({...form, image: e.target.value})
             }
@@ -59,6 +72,7 @@ export default function Home() {
           <button
             type="submit"
             onClick={createTeam}
+            className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
           >Create</button>
         </form>
       </div>
