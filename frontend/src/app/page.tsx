7bf1dd5f8b8bcfa91c3d065ea0d08a6b6073543e
Matheus Jorge <bs.fenixfl@gmail.com>
2023-08-22 02:02:35 -0300

    Feat: Att fetch data

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,28 +1,35 @@
-"use client";
-
 import { Team } from '@/interfaces/Team';
 import Image from 'next/image';
-import { useEffect, useState } from 'react';
 
-export default function Home() {
-  const [info, setInfo] = useState({
-    teams: [],
-    loading: true
-  })
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await (await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL_DEV}api/list/teams`)).json()
-      setInfo({
-        teams: data,
-        loading: false
-      })
-    }
-    fetchData()
-  }, []);
+const getData = async () => {
+  const data = await (await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL_DEV}api/list/teams`)).json()
+
+  return data
+}
+
+export default async function Home() {
+
+  const teams: Team[] = await getData()
+
+  // const [info, setInfo] = useState({
+  //   teams: [],
+  //   loadingTeams: true,
+  //   loadingPlayers: true,
+  // })
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const data = await (await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL_DEV}api/list/teams`)).json()
+  //     setInfo({
+  //       teams: data,
+  //       loadingTeams: false,
+  //       loadingPlayers: false,
+  //     })
+  //   }
+  //   fetchData()
+  // }, []);
 
   return (
     <>
-    {info.loading ? <p>Loading...</p> :
     <div>
       <h1 className='text-center text-3xl mt-4'>ProPlayers.Net</h1>
       <div className='mt-4 w-3/4 m-auto'>
@@ -35,7 +42,7 @@ export default function Home() {
               <h3 className='text-center'>North America</h3>
             </div>
             <div className='mt-3'>
-              {info.teams.filter((e: Team) => e.country === 'North America').map((team: Team) => (
+              {teams.filter((e: Team) => e.country === 'North America').map((team: Team) => (
                 <div key={team.id} className='flex'>
                   <span className='w-5 mr-3 flex justify-center items-center'>
                     <Image
@@ -55,7 +62,7 @@ export default function Home() {
               <h3 className='text-center'>Brazil</h3>
             </div>
             <div className='mt-3'>
-              {info.teams.filter((e: Team) => e.country === 'Brazil').map((team: Team) => (
+              {teams.filter((e: Team) => e.country === 'Brazil').map((team: Team) => (
                 <div key={team.id} className='flex'>
                   <span className='w-5 mr-3 flex justify-center items-center'>
                     <Image
@@ -75,7 +82,7 @@ export default function Home() {
               <h3 className='text-center'>Europe</h3>
             </div>
             <div className='mt-3'>
-              {info.teams.filter((e: Team) => e.country === 'Europe').map((team: Team) => (
+              {teams.filter((e: Team) => e.country === 'Europe').map((team: Team) => (
                 <div key={team.id} className='flex'>
                   <span className='w-5 mr-3 flex justify-center items-center'>
                     <Image
@@ -95,7 +102,7 @@ export default function Home() {
               <h3 className='text-center'>Korea</h3>
             </div>
             <div className='mt-3'>
-              {info.teams.filter((e: Team) => e.country === 'Korea').map((team: Team) => (
+              {teams.filter((e: Team) => e.country === 'Korea').map((team: Team) => (
                 <div key={team.id} className='flex'>
                   <span className='w-5 mr-3 flex justify-center items-center'>
                     <Image
@@ -111,11 +118,11 @@ export default function Home() {
             </div>
           </div>
           <div className='w-52 mt-3 border border-black rounded-lg px-3 pb-3 h-fit'>
-            <div className='pt-2 border-b-2 border-black'>
-              <h3 className='text-center'>China</h3>
+          <div className='pt-2 border-b-2 border-black'>
+              <h3 className='text-center'>Latin America</h3>
             </div>
             <div className='mt-3'>
-              {info.teams.filter((e: Team) => e.country === 'China').map((team: Team) => (
+              {teams.filter((e: Team) => e.country === 'Latin America').map((team: Team) => (
                 <div key={team.id} className='flex'>
                   <span className='w-5 mr-3 flex justify-center items-center'>
                     <Image
@@ -135,7 +142,27 @@ export default function Home() {
               <h3 className='text-center'>Asia Pacific</h3>
             </div>
             <div className='mt-3'>
-              {info.teams.filter((e: Team) => e.country === 'Asia Pacific').map((team: Team) => (
+              {teams.filter((e: Team) => e.country === 'Asia Pacific').map((team: Team) => (
+                <div key={team.id} className='flex'>
+                  <span className='w-5 mr-3 flex justify-center items-center'>
+                    <Image
+                    src={team.image}
+                    alt={team.name}
+                    width={20}
+                    height={20}
+                    />
+                  </span>
+                  <p>{team.name}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className='w-52 mt-3 border border-black rounded-lg px-3 pb-3 h-fit'>
+            <div className='pt-2 border-b-2 border-black'>
+              <h3 className='text-center'>China</h3>
+            </div>
+            <div className='mt-3'>
+              {teams.filter((e: Team) => e.country === 'China').map((team: Team) => (
                 <div key={team.id} className='flex'>
                   <span className='w-5 mr-3 flex justify-center items-center'>
                     <Image
@@ -155,7 +182,7 @@ export default function Home() {
               <h3 className='text-center'>Vietnam</h3>
             </div>
             <div className='mt-3'>
-              {info.teams.filter((e: Team) => e.country === 'Vietnam').map((team: Team) => (
+              {teams.filter((e: Team) => e.country === 'Vietnam').map((team: Team) => (
                 <div key={team.id} className='flex'>
                   <span className='w-5 mr-3 flex justify-center items-center'>
                     <Image
@@ -175,7 +202,7 @@ export default function Home() {
               <h3 className='text-center'>Oceania</h3>
             </div>
             <div className='mt-3'>
-              {info.teams.filter((e: Team) => e.country === 'Oceania').map((team: Team) => (
+              {teams.filter((e: Team) => e.country === 'Oceania').map((team: Team) => (
                 <div key={team.id} className='flex'>
                   <span className='w-5 mr-3 flex justify-center items-center'>
                     <Image
@@ -195,7 +222,7 @@ export default function Home() {
               <h3 className='text-center'>Japan</h3>
             </div>
             <div className='mt-3'>
-              {info.teams.filter((e: Team) => e.country === 'Japan').map((team: Team) => (
+              {teams.filter((e: Team) => e.country === 'Japan').map((team: Team) => (
                 <div key={team.id} className='flex'>
                   <span className='w-5 mr-3 flex justify-center items-center'>
                     <Image
@@ -213,7 +240,11 @@ export default function Home() {
         </div>
       </div>
     </div>
-    }
+    <div className='mt-4 w-3/4 m-auto'>
+      <h2
+        className='text-center text-2xl'
+      >Best Pro Players</h2>
+    </div>
     </>
   )
 }
